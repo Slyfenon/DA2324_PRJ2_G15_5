37e6791eb2283c3f4bf3c576b14b6a0aa4e9f567
Nuno Machado <saavedra.machado.1@gmail.com>
2024-05-04 16:05:04 +0100

    First implementation of Backtracking algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
+#include <chrono>
 #include "Parser.h"
+#include "Manager.h"
 
 
 int main() {
-    Parser parser;
-    Graph* graph = new Graph();
-    parser.readToyGraph("tourism.csv", graph);
-    std::cout << "------------------------------------------------------------" << std::endl;
+    Parser parser = Parser();
+    Manager manager = Manager();
+
+    parser.readToyGraph("stadiums.csv", manager.getGraph());
+    //std::cout << "------------------------------------------------------------" << std::endl;
     //parser.readToyGraph("stadiums.csv");
     //std::cout << "------------------------------------------------------------" << std::endl;
     //parser.readToyGraph("tourism.csv", graph);
+
+    for(auto v : manager.getGraph()->getVertexSet()){
+        v->setVisited(false);
+    }
+
+    std::vector<int> path;
+    path.push_back(0);
+    std::vector<int> bestPath;
+    manager.getGraph()->findVertex(0)->setVisited(true);
+    double minCost = std::numeric_limits<double>::max();
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    manager.tsp_backtracking(path, bestPath, minCost, 0.0);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    for(auto v : bestPath){
+        std::cout << v << " ";
+    }
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+
+
     return 0;
 }
