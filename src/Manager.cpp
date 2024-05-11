6d857748412b8701624fd413f0d841aa7dc21701
Nuno Machado <saavedra.machado.1@gmail.com>
2024-05-11 16:34:56 +0100

    firts implementation triangular inequality, does not work for unconnected graphs yet

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,6 +1,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <queue>
 #include "Manager.h"
 
 void checkAndUpdateBestPath(const std::vector<int>& path, std::vector<int>& bestPath, double& minCost, double currentCost, int firstVertex, Graph* graph) {
@@ -102,5 +103,116 @@ std::vector<int> &Manager::backtracking() {
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     return bestPath;
+}
+
+struct PrimVertexInfo {
+    int id;
+    double key;
+    int parent;
+};
+
+struct CompareVertexKeys {
+    bool operator()(const PrimVertexInfo& v1, const PrimVertexInfo& v2) {
+        return v1.key > v2.key;
+    }
+};
+
+
+void Manager::preOrderWalk(std::vector<Edge*> MST, Vertex* v, std::vector<Vertex*> &preOrder) {
+    v->setVisited(true);
+    preOrder.push_back(v);
+
+    for (Edge* edge : v->getAdj()) {
+        Vertex* w = graph->findVertex(edge->getDest());
+        if (!w->isVisited()) {
+            for (Edge* mstEdge : MST) {
+                if ((mstEdge->getOrig() == v->getId() && mstEdge->getDest() == w->getId()) ||
+                    (mstEdge->getOrig() == w->getId() && mstEdge->getDest() == v->getId())) {
+                    preOrderWalk(MST, w, preOrder);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+std::vector<Edge*> Manager::primMST() {
+    auto vertexSet = graph->getVertexSet();
+    unsigned long numVertices = vertexSet.size();
+
+    std::vector<bool> inMST(numVertices, false);
+    std::vector<PrimVertexInfo> primInfo(numVertices);
+    std::vector<Edge*> MSTEdges(numVertices - 1);
+
+    for (auto& entry : vertexSet) {
+        int id = entry.first;
+        primInfo[id].id = id;
+        primInfo[id].key = std::numeric_limits<double>::max();
+        primInfo[id].parent = -1;
+    }
+
+    primInfo[0].key = 0;
+
+    std::priority_queue<PrimVertexInfo, std::vector<PrimVertexInfo>, CompareVertexKeys> pq;
+    for (int i = 0; i < numVertices; ++i)
+        pq.push(primInfo[i]);
+
+    while (!pq.empty()) {
+        PrimVertexInfo u = pq.top();
+        pq.pop();
+
+        inMST[u.id] = true;
+
+        for (Edge* edge : vertexSet[u.id]->getAdj()) {
+            int v = edge->getDest();
+            if (!inMST[v] && edge->getWeight() < primInfo[v].key) {
+                primInfo[v].parent = u.id;
+                primInfo[v].key = edge->getWeight();
+                pq.push(primInfo[v]);
+            }
+        }
+    }
+
+    int edgeIndex = 0;
+    for (int i = 1; i < numVertices; ++i) {
+        int parent = primInfo[i].parent;
+        for (Edge* edge : vertexSet[i]->getAdj()) {
+            if (edge->getDest() == parent) {
+                MSTEdges[edgeIndex++] = edge;
+                break;
+            }
+        }
+    }
+
+    return MSTEdges;
+
+}
+
+void Manager::triangularInequality() {
+    std::vector<Edge*> MSTTree;
+    std::vector<Vertex*> preOrder;
+    for(auto v : graph->getVertexSet()){
+        v.second->setVisited(false);
+    }
+
+    MSTTree = primMST();
+    for(auto v : graph->getVertexSet()){
+        v.second->setVisited(false);
+    }
+
+    Vertex* root = graph->getVertexSet().find(0)->second;
+    preOrderWalk(MSTTree, root, preOrder);
+
+    preOrder.push_back(root);
+
+    int count = 0;
+    for(auto v : preOrder){
+        count++;
+        std::cout << v->getId() << " ";
+    }
+    std::cout << std::endl;
+    std::cout << count <<std::endl;
 }
 
