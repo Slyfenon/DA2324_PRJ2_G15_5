41228032e5654072f531cf17d6a680ba70ac521f
oxi-moron <rafael.t.m.030903@gmail.com>
2024-05-14 17:33:28 +0100

    added Doxyfile

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['comparevertexkeys_0',['CompareVertexKeys',['../structCompareVertexKeys.html',1,'']]],
+  ['coordinate_1',['Coordinate',['../classCoordinate.html',1,'']]]
+];
---
+++ docs/html/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['parser_0',['Parser',['../classParser.html',1,'']]],
+  ['primvertexinfo_1',['PrimVertexInfo',['../structPrimVertexInfo.html',1,'']]]
+];
